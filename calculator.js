896be61e8066448e06b7b4cc31a638019daf712b
Leon <gomdole32@naver.com>
2025-10-15 18:42:55 +0900

    유료 버전 함수 수정

--- calculator.js
+++ calculator.js
@@ -22,8 +22,13 @@ function getDivide(a, b) {
 // 추후 유료 버전 패치 예정입니다.
 
 const Today = new Date();
-console.log(`패치 날짜: ${Today}, 패치 버전: 1.1.0`)
-// squre
-function getSqure(a) {
-  return a * a; 
+console.log(`패치 날짜: ${Today}, 패치 버전: 1.1.0`);
+
+// if sum
+function getIfSum(a, b) {
+  if (a > b) {
+    return a + b;
+  } else {
+    return 'a값이 유효하지 않습니다. 다시 입력해 주세요.';
+  }
 }
